1db8b4d20db53f6cd56b5a0fbf83a22d134b7b92
Casey Shey <cshey@uw.edu>
2016-01-06 21:26:31 -0800

    minor deploy change

--- deploy.cmd
+++ deploy.cmd
@@ -103,7 +103,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install bower
-  call :ExecuteCmd  node_modules/.bin/bower install
+  call node_modules/.bin/bower install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
