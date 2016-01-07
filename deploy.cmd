578018cb193b5cfa2c60b60ea53413245512b6fb
Casey Shey <cshey@uw.edu>
2016-01-06 21:58:48 -0800

    basic deploy

--- deploy.cmd
+++ deploy.cmd
@@ -88,10 +88,10 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 2. Select node version
+echo 1. Select node version
 call :SelectNodeVersion
 
-:: 3. Install npm packages
+echo 2. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
@@ -99,16 +99,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-:: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install bower
-  call node_modules/.bin/bower install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-:: 1. KuduSync
+echo 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
