e5c1e9eb09b0b9ee5bf75e5ff28673f165411815
Casey Shey <cshey@uw.edu>
2016-06-14 21:49:35 -0700

    fix linkgearrequests cap issue

--- public/modules/linkgearrequests/config/linkgearrequests.client.routes.js
+++ public/modules/linkgearrequests/config/linkgearrequests.client.routes.js
@@ -7,19 +7,19 @@ angular.module('linkGearRequests').config(['$stateProvider',
 		$stateProvider.
 		state('listLinkGearRequests', {
 			url: '/linkGearRequests',
-			templateUrl: 'modules/linkGearRequests/views/list-linkGearRequests.client.view.html'
+			templateUrl: 'modules/linkgearrequests/views/list-linkgearrequests.client.view.html'
 		}).
 		state('createLinkGearRequest', {
 			url: '/linkGearRequests/create/:proId',
-			templateUrl: 'modules/linkGearRequests/views/create-linkGearRequest.client.view.html'
+			templateUrl: 'modules/linkgearrequests/views/create-linkgearrequests.client.view.html'
 		}).
 		state('viewLinkGearRequest', {
 			url: '/linkGearRequests/:linkGearRequestId',
-			templateUrl: 'modules/linkGearRequests/views/view-linkGearRequest.client.view.html'
+			templateUrl: 'modules/linkgearrequests/views/view-linkgearrequests.client.view.html'
 		}).
 		state('editLinkGearRequest', {
 			url: '/linkGearRequests/:linkGearRequestId/edit',
-			templateUrl: 'modules/linkGearRequests/views/edit-linkGearRequest.client.view.html'
+			templateUrl: 'modules/linkgearrequests/views/edit-linkgearrequests.client.view.html'
 		});
 	}
 ]);
