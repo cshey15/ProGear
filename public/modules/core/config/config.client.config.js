dff6cae4a155b7f3cb11a0951612f3b081f73711
Casey Shey <cshey@uw.edu>
2016-06-14 21:59:47 -0700

    route fix

--- public/modules/core/config/config.client.config.js
+++ public/modules/core/config/config.client.config.js
@@ -5,7 +5,7 @@ angular.module('core').run(['Menus',
 	function(Menus) {
         // Set top bar menu items
 		Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', '/linkGearRequests(/create)?', false, ['admin'], 1);
-        Menus.addSubMenuItem('topbar', 'admin', 'List LinkGearRequests', 'linkGearRequests');
+        Menus.addSubMenuItem('topbar', 'admin', 'List LinkGearRequests', 'linkgearrequests');
         Menus.addSubMenuItem('topbar', 'admin', 'List Issues', 'issues');
         Menus.addSubMenuItem('topbar', 'admin', 'Bulk Add', 'admin/bulkupload');
         Menus.addSubMenuItem('topbar', 'admin', 'List Unpublished pros', 'admin/pros?unpublishedonly=true');
